a126ea941c4962aa1469895af7be3e8c5c954c80
Kaito Sawada <75603046+kaitosawada@users.noreply.github.com>
2024-10-01 17:07:10 +0900

    update

--- home.nix
+++ home.nix
@@ -33,16 +33,13 @@ in
 
   programs.starship = {
     enable = true;
-    # Configuration written to ~/.config/starship.toml
     settings = {
       add_newline = true;
 
       character = {
-        success_symbol = "[$](bold green)";
-        error_symbol = "[$](bold red)";
+        success_symbol = "[\\$](bold green)";
+        error_symbol = "[\\$](bold red)";
       };
-
-      # package.disabled = true;
     };
   };
 
