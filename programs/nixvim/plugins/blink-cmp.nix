c51dca526018964930eb27a800f13b713b9afa5d
kaitosawada <kaito.sawada@proton.me>
2025-10-09 18:17:56 +0900

    update

--- programs/nixvim/plugins/blink-cmp.nix
+++ programs/nixvim/plugins/blink-cmp.nix
@@ -2,5 +2,39 @@
   # https://nix-community.github.io/nixvim/plugins/blink-cmp/index.html
   plugins.blink-cmp = {
     enable = true;
+    settings = {
+      keymap = {
+        "<C-Enter>" = [
+          "select_and_accept"
+          {
+            __raw = ''
+              function(cmp) cmp.show({ providers = { "codeium" } }) end
+            '';
+          }
+          "fallback"
+        ];
+        "<Tab>" = [
+          "select_and_accept"
+          "fallback"
+        ];
+      };
+      sources = {
+        default = [
+          "lsp"
+          "path"
+          "snippets"
+          "buffer"
+          "codeium"
+        ];
+        providers = {
+          codeium = {
+            name = "Codeium";
+            module = "codeium.blink";
+            async = true;
+            score_offset = -7;
+          };
+        };
+      };
+    };
   };
 }
--- programs/nixvim/plugins/windsurf-vim.nix
+++ programs/nixvim/plugins/windsurf-vim.nix
@@ -1,3 +1,10 @@
 {
-  plugins.windsurf-vim.enable = true;
+  # blink-cmpに対応しているのがwindsurf.nvimだったので利用している
+  # windsurf.vimの方が微妙に開発が活発かもしれない
+  plugins.windsurf-nvim = {
+    enable = true;
+    settings = {
+      enable_cmp_source = false;
+    };
+  };
 }
