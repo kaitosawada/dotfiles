8bb987849c4b038a81ec77e5d99dc3653aa2303f
kaitosawada <mitosawada@gmail.com>
2026-04-25 14:34:34 +0900

    feat: lazygit を Zellij パイプ経由で Neovim に統合
    lazygit の edit コマンドを Zellij セッションパイプ経由で
    Neovim サーバーに接続するように更新した。従来の $NVIM
    環境変数ベースの複雑なリモートコマンド実行から、
    /tmp/nvim-$ZELLIJ_SESSION_NAME.pipe を使用したシンプルな
    パイプ通信に変更。これにより Zellij 統合を強化し、
    複数のセッション間でファイル編集がスムーズに行われるように
    なった。Zellij のキーバインド設定から lazygit の浮動
    ウィンドウ起動を削除。

--- programs/lazygit.nix
+++ programs/lazygit.nix
@@ -3,8 +3,10 @@
     enable = true;
     settings = {
       os = {
-        edit = "nvim --server \"$NVIM\" --remote-send '<C-\\><C-n>:let buf=bufnr()<CR>:close<CR>:execute \"bd! \" . buf<CR>:execute \"edit \" . fnameescape({{filename}})<CR>'";
-        editAtLine = "nvim --server \"$NVIM\" --remote-send '<C-\\><C-n>:let buf=bufnr()<CR>:close<CR>:execute \"bd! \" . buf<CR>:execute \"edit +\" . {{line}} . \" \" . fnameescape({{filename}})<CR>'";
+        edit = "nvim --server /tmp/nvim-$ZELLIJ_SESSION_NAME.pipe --remote {{filename}}";
+        editAtLine = "nvim --server /tmp/nvim-$ZELLIJ_SESSION_NAME.pipe --remote-send ':edit +{{line}} {{filename}}<CR>'";
+        # edit = "nvim --server \"$NVIM\" --remote-send '<C-\\><C-n>:let buf=bufnr()<CR>:close<CR>:execute \"bd! \" . buf<CR>:execute \"edit \" . fnameescape({{filename}})<CR>'";
+        # editAtLine = "nvim --server \"$NVIM\" --remote-send '<C-\\><C-n>:let buf=bufnr()<CR>:close<CR>:execute \"bd! \" . buf<CR>:execute \"edit +\" . {{line}} . \" \" . fnameescape({{filename}})<CR>'";
       };
       customCommands = [
         {
