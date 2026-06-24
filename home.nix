8c235824b2ac7ac88354b9ded8903d0b89bb3d90
kaitosawada <75603046+kaitosawada@users.noreply.github.com>
2026-06-24 14:28:55 +0900

    cursor

--- home.nix
+++ home.nix
@@ -85,8 +85,8 @@ in
         # llm
         # inputs.nix-claude-code.packages.${system}.claude
         # inputs.opencode.packages.${system}.default
+        inputs.llm-agents.packages.${system}.cursor-agent
         github-copilot-cli
-        cursor-cli
 
         # bitwarden
         bitwarden-cli
