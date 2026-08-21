c86ee798ef07e7d5edd66c68584d0254b0ad2981
kaitosawada <75603046+kaitosawada@users.noreply.github.com>
2026-08-21 15:02:32 +0900

    git: use PAT for Forgejo over HTTPS

--- programs/git.nix
+++ programs/git.nix
@@ -1,8 +1,19 @@
-{ username, ... }:
+{ config, username, ... }:
 let
   email = "75603046+kaitosawada@users.noreply.github.com";
 in
 {
+  sops.secrets."forgejo-token" = {
+    sopsFile = ../secrets/home.yaml;
+  };
+
+  sops.templates."git-credentials-forgejo" = {
+    content = ''
+      https://k.sawada:${config.sops.placeholder."forgejo-token"}@git.ozonehl.dev
+    '';
+    path = "${config.home.homeDirectory}/.config/git/credentials-forgejo";
+  };
+
   programs.git = {
     enable = true;
     settings = {
@@ -16,6 +27,13 @@ in
       pull.rebase = true;
       fetch.prune = true;
       ghq.root = "~/ghq";
+      url."https://git.ozonehl.dev/".insteadOf = [
+        "git@git.ozonehl.dev:"
+        "ssh://git@git.ozonehl.dev/"
+      ];
+      credential."https://git.ozonehl.dev".helper = "store --file ${
+        config.sops.templates."git-credentials-forgejo".path
+      }";
     };
   };
 }
